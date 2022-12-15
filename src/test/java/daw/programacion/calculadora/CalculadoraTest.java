1570ecb9eadfaf6fd549000a8aa56c1ebc60a820
Zijun Hu <zijun.hu@tiernoparla.es>
2022-12-15 17:30:54 +0100

    CalculadoraTest

--- src/test/java/daw/programacion/calculadora/CalculadoraTest.java
+++ src/test/java/daw/programacion/calculadora/CalculadoraTest.java
@@ -14,5 +14,47 @@ public class CalculadoraTest {
 
         assertEquals(expected, calc.sumar(numero1, numero2));
     } //dosSumarDisEsCuatro
+
+    @Test
+    public void cincoSumarSieteEsDoce() {
+
+        int expected = 12;
+        int numero1 = 5, numero2 = 7;
+        Calculadora calc = new Calculadora();
+
+        assertEquals(expected, calc.sumar(numero1, numero2));
+    } //cincoSumarSieteEsDoce
+
+    @Test
+    public void sieteSumarSieteEsCatorce() {
+
+        int expected = 14;
+        int numero1 = 7, numero2 = 7;
+        Calculadora calc = new Calculadora();
+
+        assertTrue("siete mas cinco deberia ser 14", expected == calc.sumar(numero1, numero2));
+    }
+
+    @Test
+    public void tresSumarSieteEsDiez() {
+
+        int expected = 10;
+        int numero1 = 3, numero2 = 7;
+        Calculadora calc = new Calculadora();
+
+        assertTrue("tres mas siete deberia ser 10", expected == calc.sumar(numero1, numero2));
+    } //tresSumarSieteEsDiez
+
+    @Test
+    public void tresSumarSieteNoEsCien() {
+
+        int expected = 100;
+        int numero1 = 3, numero2 = 7;
+        Calculadora calc = new Calculadora();
+
+        assertFalse("Tres mas siete no deberia ser 100", expected == calc.sumar(numero1, numero2));
+    } //tresSumarSieteNoEsCien
+
+    
     
 } //CalculadoraTest
